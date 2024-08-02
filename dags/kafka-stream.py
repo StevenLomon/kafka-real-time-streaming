76d08d3970316c1db2c14fe4360f427ee371bf64
StevenLomon <steven.lennartsson@gmail.com>
2024-08-03 01:56:38 +0200

    stream_data functino fully implemented

--- dags/kafka-stream.py
+++ dags/kafka-stream.py
@@ -1,4 +1,4 @@
-import requests, json, time
+import requests, json, time, logging
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
@@ -37,13 +37,23 @@ def format_data(response): # Format data for the Kafka queue; streamline our dat
     return data
 
 def stream_data(): 
-    res = get_data()
-    res = format_data(res)
-    # print(json.dumps(res, indent=3))
-
     # Publish and push the data to the Kafka queue
     producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000) # timeout
-    producer.send('users_created', json.dumps(res).encode('utf-8'))
+    curr_time = time.time()
+
+    while True:
+        if time.time() > curr_time + 60: # While the time is between 1 minute; while we are producing between 1 and 60 seconds
+            break
+        try:
+            # Get the data; send as many requests to the API as possible
+            res = get_data()
+            res = format_data(res)
+            # print(json.dumps(res, indent=3))
+             
+            producer.send('users_created', json.dumps(res).encode('utf-8'))
+        except Exception as e:
+            logging.error(f'An error occured: {e}')
+            continue # Continue the loop and if one minute pass, we break
     
 
 with DAG(
