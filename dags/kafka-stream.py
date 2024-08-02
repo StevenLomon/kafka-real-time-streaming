24ecd1f95ae08e5662017fce6bd95b5d8aed9630
StevenLomon <steven.lennartsson@gmail.com>
2024-08-03 01:53:03 +0200

    Fixed kafka import error

--- dags/kafka-stream.py
+++ dags/kafka-stream.py
@@ -3,12 +3,11 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from kafka import KafkaProducer
-from confluent_kafka import Producer
 # from rich import print
 
 default_args = {
     'owner': 'airscholar Steve',
-    'start_date': datetime(2024, 7, 25)
+    'start_date': datetime(2024, 8, 3)
 }
 
 def get_data():
@@ -42,21 +41,20 @@ def stream_data():
     res = format_data(res)
     # print(json.dumps(res, indent=3))
 
-    # Publish and push the data to the Kafka queue (?)
-    # We are not yet in the Docker container, hence we use localhost instead of 'broker:29092'
-    producer = KafkaProducer(bootstrap_servers=['localhost:9092'], max_block_ms=5000) # timeout
+    # Publish and push the data to the Kafka queue
+    producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000) # timeout
     producer.send('users_created', json.dumps(res).encode('utf-8'))
     
 
-# with DAG(
-#     'user_automation',
-#     default_args=default_args,
-#     schedule_interval='@daily',
-#     catchup=False) as dag:
+with DAG(
+    'user_automation',
+    default_args=default_args,
+    schedule_interval='@daily',
+    catchup=False) as dag:
     
-#         streaming_task = PythonOperator(
-#                 task_id='stream_data_from_api',
-#                 python_callable=stream_data
-#         )
+        streaming_task = PythonOperator(
+                task_id='stream_data_from_api',
+                python_callable=stream_data
+        )
 
 stream_data()
