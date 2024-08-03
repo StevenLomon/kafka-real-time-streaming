0e0e6a7fa8e655a934f3b9d75b3b24dc175c21cf
StevenLomon <steven.lennartsson@gmail.com>
2024-08-03 05:01:15 +0200

    Final touches!

--- spark_stream.py
+++ spark_stream.py
@@ -1,42 +1,40 @@
 import logging
-from datetime import datetime
 
-from cassandra.auth import PlainTextAuthProvider
 from cassandra.cluster import Cluster
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col
 from pyspark.sql.types import StructType, StructField, StringType
 
-# A keyspace is like a schema
 
 def create_keyspace(session):
     session.execute("""
         CREATE KEYSPACE IF NOT EXISTS spark_streams
-        WITH replication = {'class': 'SimpleStrategy', 'replication_factor: '1'};
+        WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};
     """)
 
     print("Keyspace created successfully!")
 
+
 def create_table(session):
     session.execute("""
-        CREATE TABLE IF NOT EXISTS spark_streams.created_users (
-            id UUID PRIMARY KEY,
-            first_name TEXT,
-            last_name TEXT,
-            gender TEXT,
-            address TEXT,
-            post_code TEXT,
-            email TEXT,
-            username TEXT,
-            registered_date TEXT,
-            phone TEXT,
-            picture TEXT);
+    CREATE TABLE IF NOT EXISTS spark_streams.created_users (
+        id UUID PRIMARY KEY,
+        first_name TEXT,
+        last_name TEXT,
+        gender TEXT,
+        address TEXT,
+        post_code TEXT,
+        email TEXT,
+        username TEXT,
+        registered_date TEXT,
+        phone TEXT,
+        picture TEXT);
     """)
 
     print("Table created successfully!")
 
+
 def insert_data(session, **kwargs):
-    # Extract data from kwargs
     print("inserting data...")
 
     user_id = kwargs.get('id')
@@ -62,7 +60,7 @@ def insert_data(session, **kwargs):
         logging.info(f"Data inserted for {first_name} {last_name}")
 
     except Exception as e:
-        logging.error(f'could not insert data due to {e}')
+        logging.error(f'Could not insert data due to exception: {e}')
 
 
 def create_spark_connection():
@@ -77,14 +75,14 @@ def create_spark_connection():
             .getOrCreate()
 
         s_conn.sparkContext.setLogLevel("ERROR")
-        logging.info("Spark connection created successfully :))")
+        logging.info("Spark connection created successfully!")
     except Exception as e:
-        logging.error(f"Couldn't create the spark session due to exception {e}")
+        logging.error(f"Couldn't create the spark session due to exception: {e}")
 
     return s_conn
 
+
 def connect_to_kafka(spark_conn):
-    # We're using Spark to read data from Kafka
     spark_df = None
     try:
         spark_df = spark_conn.readStream \
@@ -93,26 +91,27 @@ def connect_to_kafka(spark_conn):
             .option('subscribe', 'users_created') \
             .option('startingOffsets', 'earliest') \
             .load()
-        logging.info("Kafka dataframe created successfully :))")
+        logging.info("kafka dataframe created successfully")
     except Exception as e:
         logging.warning(f"Kafka dataframe could not be created due to exception: {e}")
 
     return spark_df
-            
+
+
 def create_cassandra_connection():
-    # Connect to the Cassandra cluster
     try:
+        # connecting to the cassandra cluster
         cluster = Cluster(['localhost'])
+
         cas_session = cluster.connect()
 
         return cas_session
-
     except Exception as e:
-        logging.error(f"Couldn't create Cassandra connection due to exception: {e}")
+        logging.error(f"Could not create Cassandra connection due to exceptoin: {e}")
         return None
-    
-def structure_kafka_df_for_cassandra(spark_df):
-    # We structure the data from the dataframe by selecting only a certain piece from it
+
+
+def create_selection_df_from_kafka(spark_df):
     schema = StructType([
         StructField("id", StringType(), False),
         StructField("first_name", StringType(), False),
@@ -133,27 +132,27 @@ def structure_kafka_df_for_cassandra(spark_df):
 
     return sel
 
+
 if __name__ == "__main__":
+    # create spark connection
     spark_conn = create_spark_connection()
 
     if spark_conn is not None:
-        # Connect to Kafka with Spark connection
+        # connect to kafka with spark connection
         spark_df = connect_to_kafka(spark_conn)
-        # Structure datafram for insertion into Cassandra
-        selection_df = structure_kafka_df_for_cassandra(spark_df)
+        selection_df = create_selection_df_from_kafka(spark_df)
         session = create_cassandra_connection()
 
         if session is not None:
             create_keyspace(session)
             create_table(session)
-            # insert_data(session)
 
-            # We don't just wanna insert one, we want to insert many through a stream
+            logging.info("Streaming is being started...")
+
             streaming_query = (selection_df.writeStream.format("org.apache.spark.sql.cassandra")
-                                .option('checkpoitnLocation', '/tmp/checkpoint')
-                                .option('keyspace', 'spark_streams')
-                                .option('table', 'created_users')
-                                .start())
-            
+                               .option('checkpointLocation', '/tmp/checkpoint')
+                               .option('keyspace', 'spark_streams')
+                               .option('table', 'created_users')
+                               .start())
+
             streaming_query.awaitTermination()
-                            
