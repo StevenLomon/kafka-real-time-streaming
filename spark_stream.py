d35502e060b9dfaabdf840b5822dae8aa4c7e7aa
StevenLomon <steven.lennartsson@gmail.com>
2024-08-03 04:04:36 +0200

    connect_to_kafka function implemented

--- spark_stream.py
+++ spark_stream.py
@@ -9,13 +9,60 @@ from pyspark.sql.functions import from_json, col
 # A keyspace is like a schema
 
 def create_keyspace(session):
-    pass
+    session.execute("""
+        CREATE KEYSPACE IF NOT EXISTS spark_streams
+        WITH replication = {'class': 'SimpleStrategy', 'replication_factor: '1'};
+    """)
+
+    print("Keyspace created successfully!")
 
 def create_table(session):
-    pass
+    session.execute("""
+        CREATE TABLE IF NOT EXISTS spark_streams.created_users (
+            id UUID PRIMARY KEY,
+            first_name TEXT,
+            last_name TEXT,
+            gender TEXT,
+            address TEXT,
+            post_code TEXT,
+            email TEXT,
+            username TEXT,
+            registered_date TEXT,
+            phone TEXT,
+            picture TEXT);
+    """)
+
+    print("Table created successfully!")
 
 def insert_data(session, **kwargs):
-    pass
+    # Extract data from kwargs
+    print("inserting data...")
+
+    user_id = kwargs.get('id')
+    first_name = kwargs.get('first_name')
+    last_name = kwargs.get('last_name')
+    gender = kwargs.get('gender')
+    address = kwargs.get('address')
+    postcode = kwargs.get('post_code')
+    email = kwargs.get('email')
+    username = kwargs.get('username')
+    dob = kwargs.get('dob')
+    registered_date = kwargs.get('registered_date')
+    phone = kwargs.get('phone')
+    picture = kwargs.get('picture')
+
+    try:
+        session.execute("""
+            INSERT INTO spark_streams.created_users(id, first_name, last_name, gender, address, 
+                post_code, email, username, dob, registered_date, phone, picture)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """, (user_id, first_name, last_name, gender, address,
+              postcode, email, username, dob, registered_date, phone, picture))
+        logging.info(f"Data inserted for {first_name} {last_name}")
+
+    except Exception as e:
+        logging.error(f'could not insert data due to {e}')
+
 
 def create_spark_connection():
     s_conn = None
@@ -34,6 +81,22 @@ def create_spark_connection():
         logging.error(f"Couldn't create the Spark session due to exception: {e}")
 
     return s_conn
+
+def connect_to_kafka(spark_conn):
+    # We're using Spark to read data from Kafka
+    spark_df = None
+    try:
+        spark_df = spark_conn.readStream \
+            .format('kafka') \
+            .option('kafka.bootstrap.servers', 'localhost:9092') \
+            .option('subscribe', 'users_created') \
+            .option('startingOffsets', 'earliest') \
+            .load()
+        logging.info("Kafka dataframe created successfully :))")
+    except Exception as e:
+        logging.warning(f"Kafka dataframe could not be created due to exception: {e}")
+
+    return spark_df
             
 def create_cassandra_connection():
     # Connect to the Cassandra cluster
@@ -53,3 +116,7 @@ if __name__ == "__main__":
 
     if spark_conn is not None:
         session = create_cassandra_connection()
+
+        if session is not None:
+            create_keyspace(session)
+            create_table(session)
